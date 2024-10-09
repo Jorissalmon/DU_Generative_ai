b5d9cb8dfcaf4e946409130153db0b4f03de47b0
Jorissalmon <joris.salmon53290@gmail.com>
2024-10-09 15:24:53 +0200

    update librairies

--- app.py
+++ app.py
@@ -4,14 +4,13 @@ import os
 from dotenv import load_dotenv
 from langchain.chains import RetrievalQA
 from langchain.chat_models import ChatOpenAI
-from langchain.embeddings.openai import OpenAIEmbeddings
-from langchain.vectorstores import Chroma
 from langchain.text_splitter import RecursiveCharacterTextSplitter
-from langchain.document_loaders import PyPDFLoader
 from langchain.memory import ConversationBufferMemory
 from langchain.chains import ConversationalRetrievalChain
 from langchain.prompts import PromptTemplate
-
+from langchain_community.embeddings import OpenAIEmbeddings
+from langchain_community.vectorstores import Chroma
+from langchain_community.document_loaders import PyPDFLoader
 # 1. Initialiser l'API OpenAI
 load_dotenv()  # Charger les variables depuis le fichier .env
 #openai_api_key = os.getenv("OPENAI_API_KEY")  # Récupérer la clé API d'OpenAI
